275c7320737e589ca2f085942f3745d329555a78
vishal <vishalninaniya8949info@gmail.com>
2024-01-15 11:12:13 +0530

    blog routes updated

--- routes/blog.js
+++ routes/blog.js
@@ -2,7 +2,6 @@ import express from "express";
 import multer from "multer";
 import { storage1 } from "../services/file-handle.js";
 import { Blog } from "../models/blogSchema.js";
-import { User } from "../models/userSchema.js";
 import { postNewBlog } from "../controllers/blog.js";
 
 const blog = express.Router();
